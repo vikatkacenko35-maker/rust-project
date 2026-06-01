48612a8739bfaeda6fe32c9045b1017599850b32
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-06-01 18:04:41 +0300

    Inital commit

---
+++ target/debug/deps/Project_tkachenko-49d7d3e387f52537.d
@@ -0,0 +1,5 @@
+C:\Users\Kosmos\Documents\Project_tkachenko\target\debug\deps\Project_tkachenko-49d7d3e387f52537.d: src\main.rs
+
+C:\Users\Kosmos\Documents\Project_tkachenko\target\debug\deps\libProject_tkachenko-49d7d3e387f52537.rmeta: src\main.rs
+
+src\main.rs:
